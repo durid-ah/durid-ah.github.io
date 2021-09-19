507963731af2cab97f58bcf7ed14e590ec5aec45
durid-ah <ahmad.durid96@gmail.com>
2021-09-18 22:48:07 -0600

    importing the styles and some minor fixes

--- component/layout.tsx
+++ component/layout.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Link from "next/link";
 import React from "react";
+
 import { NavLocation } from "../models/nav_location";
+import styles from "./layout.module.css";
 
 type LayoutProps = {
    children?: React.ReactNode,
@@ -12,11 +14,11 @@ export default function Layout({children} : LayoutProps) {
 
    return (
       <>
-         <nav className="navbar">
+         <nav className={styles.navbar}>
             <ul>
-               <li><Link href="">Home</Link></li>
-               <li><Link href="">Projects</Link></li>
-               <li><Link href="">Resume</Link></li>
+               <li><Link href="/">Home</Link></li>
+               <li><Link href="/">Projects</Link></li>
+               <li><Link href="/">Resume</Link></li>
             </ul>
          </nav>
          <div>
