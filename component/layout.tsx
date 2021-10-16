c472e0ec25bb0716ce2ca8d33c5a2a547d74ac00
durid-ah <ahmad.durid96@gmail.com>
2021-10-15 19:15:33 -0600

    remove all of the app link instances in the layout component

--- component/layout.tsx
+++ component/layout.tsx
@@ -4,45 +4,19 @@ import React from "react";
 
 import { NavLocation } from "../models/nav_location";
 import styles from "./layout.module.css";
+import { AppLinks } from "./app_links";
 
 type LayoutProps = {
    children?: React.ReactNode,
    location: NavLocation
 }
 
-/**
- * Compares the current location passed into the component
- * to a target location
- * 
- * @param location the current page location
- * @param targetLocation the location that you want to compare it to
- * @returns the navButton clicked style or an empty string
- */
-const isPageClicked = (location: NavLocation, targetLocation: NavLocation) => 
-   location == targetLocation ? styles.navButtonClicked : ""; 
-
 export default function Layout({children, location} : LayoutProps) {
 
-   // when one of the the page links is clicked it will assign it the clicked
-   // class to that button adn clears it from the others
-   const homeClickedStyle = isPageClicked(location, NavLocation.Home);
-   const projectClickedStyle = isPageClicked(location, NavLocation.Projects);
-   const resumeClickedStyle = isPageClicked(location, NavLocation.Resume);
-   
    return (
       <>
          <nav className={styles.navbar}>
-            <ul className={styles.show}>
-               <li className={`${styles.navButton} ${homeClickedStyle}`}>
-                  <Link href="/">Home</Link>
-               </li>
-               <li className={`${styles.navButton} ${projectClickedStyle}`}>
-                  <Link href="/">Projects</Link>
-               </li>
-               <li className={`${styles.navButton} ${resumeClickedStyle}`}>
-                  <Link href="/">Resume</Link>
-               </li>
-            </ul>
+            <AppLinks location={location}/>
          </nav>
          <div>
             {children}
