ffbbd0bea75c73e7eadacb3b4c09b4a8625d4987
durid-ah <ahmad.durid96@gmail.com>
2021-10-21 10:05:50 -0600

    fixied the structure discrepancy in the structure of the plugins in

--- next.config.js
+++ next.config.js
@@ -1,24 +1,26 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const imageminSvgo = require('imagemin-svgo');
 
 // default next.js configuration
-const config = {
+const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 // Plugin handling
-const configWithPlugins = withPlugins([optimizedImages, {
-  handleImages: ['svg'],
-  svgo: imageminSvgo({
-    plugins: [{
-      name: 'removeViewBox',
-      active: false
-    }]
-  }),
-}], config)
+const configWithPlugins = withPlugins([
+  // image plugins
+  [optimizedImages, {
+    handleImages: ['svg'],
+    svgo: {
+      plugins: [{
+        name: 'removeViewBox',
+        active: false
+      }]
+    }
+  }]
+], nextConfig)
 
 module.exports = configWithPlugins;
