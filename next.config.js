39a3ee79e95be2670d094739631bd2c679c4220d
durid-ah <ahmad.durid96@gmail.com>
2021-10-20 22:20:18 -0600

    adding the plugin configuration for image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
-module.exports = {
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+// default next.js configuration
+const config = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-}
+};
+
+// Plugin handling
+const configWithPlugins = withPlugins([optimizedImages, {}], config)
+
+module.exports = configWithPlugins;
