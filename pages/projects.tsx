d21689c485b3ee6d4676ff322474cf41de514650
durid-ah <ahmad.durid96@gmail.com>
2021-12-17 22:35:16 -0700

    implemented the data retrieval into the projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,44 +1,30 @@
 import Head from 'next/head';
 import Layout from "../component/layout";
 import ProjectCard from '../component/project_card';
-import TechChip from '../component/tech_chip';
+import { getPortfolioData, getProjectMetadata } from '../lib/project_parser';
 import { NavLocation } from "../models/nav_location";
-import { Project } from '../models/project';
+import Project from '../models/project';
 
 import styles from "../styles/Projects.module.css";
 
-export default function Projects() {
-   const data = ["Next.js", "Node.js", "Github Actions", "Next.js", "Node.js", "Github Actions", "Next.js", "Node.js", "Github Actions", "Next.js", "Node.js", "Github Actions", "Next.js", "Node.js", "Github Actions"];
-   const projectData: Project[] = [{
-      title: "Some Programming Project",
-      description: 
-         `This will contain a very long description about how this project was set up
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-      technologies: ["Next.js", "Node.js", "Github Actions"]
-   },
-   {
-      title: "Some Programming Project",
-      description: 
-         `This will contain a very long description about how this project was set up
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-      technologies: ["Next.js", "Node.js", "Github Actions"]
-   },
-   {
-      title: "Some Programming Project",
-      description: 
-         `This will contain a very long description about how this project was set up
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-      technologies: ["Next.js", "Node.js", "Github Actions"]
-   }]
+export async function getStaticProps() {
+   const projectList = await getProjectMetadata();
+   const portfolioDescription = await getPortfolioData();
+   return {
+      props: {
+         projectList,
+         portfolioDescription
+      }
+   }
+}
+
+type ProjectProps = {
+   projectList: Project[],
+   portfolioDescription: Project
+}
+
+export default function Projects({projectList, portfolioDescription}: ProjectProps) {
+   
    return (
       <>
          <Layout location={NavLocation.Projects}>
@@ -58,20 +44,11 @@ export default function Projects() {
                      <h1>My Projects:</h1>
                   </div>
                   <h2>This Project:</h2>
-                  <div className={styles.currentProject}>
-                     <p className={styles.projectDescription}>
-                        This will contain a very long description about how this project was set up
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                     </p>
-                     <p className={styles.projectDescription}><span>Technologies Used:</span> <span className={styles.chips}>{data.map(text => (<TechChip text={text} />))}</span></p>
-                     <hr/>
-                  </div>
+                  <ProjectCard project={portfolioDescription}/>
                   <h2>Other Projects:</h2>
                   {
-                     projectData.map(item => (
-                        <ProjectCard project={item}/>
+                     projectList.map(item => (
+                        <ProjectCard key={item.title} project={item}/>
                      ))
                   }
 
