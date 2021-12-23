750ba4a6dce87debe8fbdd61d5daf7c6b573d797
durid-ah <ahmad.durid96@gmail.com>
2021-12-22 20:25:25 -0700

    set up support for jpeg images

--- next.config.js
+++ next.config.js
@@ -1,24 +1,28 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 // default next.js configuration
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 // Plugin handling
 const configWithPlugins = withPlugins([
   // image plugins
   [optimizedImages, {
-    handleImages: ['svg'],
+    handleImages: ['svg, jpeg'],
     svgo: {
       plugins: [{
         name: 'removeViewBox',
         active: false
       }]
+    },
+    mozjpeg: {
+      quality: 80
     }
   }]
 ], nextConfig);
