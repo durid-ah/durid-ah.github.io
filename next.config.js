cd661234b44e567ec92642ef80542a52312d4701
durid-ah <ahmad.durid96@gmail.com>
2021-12-29 22:06:13 -0700

    removed support for jpeg in the next config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,24 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const imageminMozjpeg = require('imagemin-mozjpeg');
 
 // default next.js configuration
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 // Plugin handling
 const configWithPlugins = withPlugins([
   // image plugins
   [optimizedImages, {
-    handleImages: ['svg, jpeg'],
+    handleImages: ['svg'],
     svgo: {
       plugins: [{
         name: 'removeViewBox',
         active: false
       }]
-    },
-    mozjpeg: {
-      quality: 80
     }
   }]
 ], nextConfig);
