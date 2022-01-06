5d2704676856cf84caf499229e1417f62abb0f68
durid-ah <ahmad.durid96@gmail.com>
2022-01-05 21:53:01 -0700

    implemented the button to get to the github repo

--- component/project_card.tsx
+++ component/project_card.tsx
@@ -1,6 +1,9 @@
 import Project from "../models/project";
-import styles from "./project_card.module.css";
 import TechChip from "./tech_chip";
+
+import styles from "./project_card.module.css";
+import { default as GitHubSvg } from "../images/iconmonstr-github-2.svg";
+import React from "react";
 
 type ProjectCardProps = {
    project: Project
@@ -22,6 +25,19 @@ const ProjectCard: React.FC<ProjectCardProps> = ({project}) => {
                }
             </span>
          </p>
+         <div className={styles.projectLinks}>
+            {
+               project.github && 
+               <a href={project.github} target="_blank" className={styles.link}>
+                  <div className={styles.linkImage}>
+                     <GitHubSvg height={24} width={24}/>
+                  </div>
+                  <div className={styles.label}>
+                     GitHub Repo
+                  </div>
+               </a>
+            }
+         </div>
       </div>
    )
 }
