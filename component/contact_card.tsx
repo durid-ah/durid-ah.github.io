d696ee017c6ef8ec35e14fc2b4c6107d0bb68691
durid-ah <ahmad.durid96@gmail.com>
2022-02-09 21:42:49 -0700

    final touches to the skills and contact me section

--- component/contact_card.tsx
+++ component/contact_card.tsx
@@ -4,13 +4,15 @@ import styles from "./contact_card.module.css";
 type ContactCardProps = {
    href: string,
    SvgIcon: React.FC<SVGProps<SVGSVGElement>>,
-   label?: string
+   label?: string,
+   background: "white" | "purple"
 }
 
-export default function ContactCard({href, SvgIcon, label}: ContactCardProps) {
-   const openLink = () => window.open(href, "_blank", "noreferrer")
+export default function ContactCard({href, SvgIcon, label, background}: ContactCardProps) {
+   const openLink = () => href != "" ? window.open(href, "_blank", "noreferrer"): "";
+   const backgroundStyle = background == "white"? styles.whiteBackground: styles.purpleBackground;
    return (
-      <div className={styles.card} onClick={openLink}>
+      <div className={`${styles.card} ${backgroundStyle}`} onClick={openLink}>
          <div className={styles.cardIcon}>
                <SvgIcon/>
          </div>
