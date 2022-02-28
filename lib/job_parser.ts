8499936bf8bc4cfe7ccf5e6346d96e646fde72e9
durid-ah <ahmad.durid96@gmail.com>
2022-02-28 09:56:28 -0700

    set up initial json parsing for the job list

--- lib/job_parser.ts
+++ lib/job_parser.ts
@@ -1,5 +1,15 @@
+import {promises as fs} from "fs";
+import path from "path";
+import { Job } from "../models/job";
 
+const DATA_DIR = '../data';
+const JOBS_FILE = 'jobs.json';
 
-export const getJobList = () => {
-   return [];
+export const getJobList = async () => {
+   let filePath = path.join(DATA_DIR, JOBS_FILE);
+   let dataString = await fs.readFile(filePath, 'utf-8');
+   let parsedData: any[] = JSON.parse(dataString);
+   console.log(dataString);
+
+   return parsedData.map(data => new Job(data));
 }
