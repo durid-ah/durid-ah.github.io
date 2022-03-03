e12429149f288e60cf708d9f609536b4e660b450
durid-ah <ahmad.durid96@gmail.com>
2022-03-02 21:18:44 -0700

    fixed some broken import paths as well as the json keys

--- component/project_components/project_card/project_card.tsx
+++ component/project_components/project_card/project_card.tsx
@@ -2,7 +2,7 @@ import Project from "../../../models/project";
 import TechChip from "../tech_chip/tech_chip";
 
 import styles from "./project_card.module.css";
-import { default as GitHubSvg } from "../../images/iconmonstr-github-2.svg";
+import { default as GitHubSvg } from "../../../images/iconmonstr-github-2.svg";
 import React from "react";
 
 type ProjectCardProps = {
