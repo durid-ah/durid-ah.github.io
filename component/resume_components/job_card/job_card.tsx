e44aef7ed603b7f3efc04461780c4d2680c542e8
durid-ah <ahmad.durid96@gmail.com>
2022-03-05 22:42:40 -0700

    finished designing the resume section

--- component/resume_components/job_card/job_card.tsx
+++ component/resume_components/job_card/job_card.tsx
@@ -1,14 +1,17 @@
 
+import { useState } from "react";
 import Job from "../../../models/job";
+import { TimeDiff } from "./job_card.model";
 import styles from "./job_card.module.css";
 
-export default function JobCard({position, company, location, descriptions, endDate}: Job) {
-   const end = endDate ?? new Date();
+export default function JobCard({position, company, location, descriptions, startDate, endDate}: Job) {
+   const parsedEndDate = endDate ? new Date(endDate): new Date();
+   const [timeDiff, _] = useState(new TimeDiff(new Date(startDate), parsedEndDate))
 
    return (
          <div className={styles.container}>
          <div className={styles.content}>
-            <h2>{position}</h2>
+            <h2> <span>{position}</span> <span className={styles.time}>{timeDiff.toString()}</span></h2>
             <h3>{company} ({location})</h3>
             <ul>
                {descriptions.map(desc => (<li key={desc}>{desc}</li>))}
